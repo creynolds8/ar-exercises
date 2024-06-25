1d469d77638977d6f3bae01e49c618258dacf3c9
creynolds8 <cameron8.cr@gmail.com>
2024-06-25 12:15:44 -0700

    complete exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,29 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+burnaby = Store.new do |t|
+  t.name = "Burnaby"
+  t.annual_revenue = 300000
+  t.mens_apparel = true
+  t.womens_apparel = true
+end
+burnaby.save
+
+richmond = Store.new do |t|
+  t.name = "Richmond"
+  t.annual_revenue = 1260000
+  t.mens_apparel = false
+  t.womens_apparel = true
+end
+richmond.save
+
+gastown = Store.new do |t|
+  t.name = "Gastown"
+  t.annual_revenue = 190000
+  t.mens_apparel = true
+  t.womens_apparel = false
+end
+gastown.save
+
+puts Store.count
